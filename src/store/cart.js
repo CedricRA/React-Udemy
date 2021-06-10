0c6fee2a991874a2f1159cd6c360262a1c125fce
CedricRA <ezsz@live.fr>
2021-06-10 15:44:40 +0200

    Add/remove items to cart

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,12 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const cartInitialState = {show: false};
+const cartInitialState = { show: false, items: [], totalQuantity: 0, totalAmout: 0 };
 const cartSlice = createSlice({
     name: 'cart',
     initialState: cartInitialState,
     reducers: {
         toggleCart(state) {
             state.show = !state.show;
+        },
+        add(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    quantity: 1,
+                    price: newItem.price,
+                    totalPrice: newItem.price,
+                    title: newItem.title
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        remove(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
         }
     }
 });
